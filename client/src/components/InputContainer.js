0007137108d2c9a5c101855034fbd86a9e6f1870
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-05-09 12:38:59 +0900

    inputBox 컴포넌트

---
+++ client/src/components/InputContainer.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+import React from 'react';
+
+const BoxStyle = styled.div`
+  width: calc(100% - 40px);
+  background-color: #F5F6F6;
+  margin : 20px;
+  height: 300px;
+  border-radius: 18px;
+  box-sizing: border-box;
+
+`;
+
+function InputContainer(){
+  return (
+    <BoxStyle>
+    </BoxStyle>
+  )
+
+}
+export default InputContainer;
+

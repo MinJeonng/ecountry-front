21a8614893b31d5e27fcf6392bfe426f687358ef
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-05-18 00:42:01 +0900

    reactquill 구현 및 firebase 연결

--- client/src/components/NewsWrite.js
+++ client/src/components/NewsWrite.js
@@ -1,16 +1,121 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ConfirmBtn } from './SettingBtn';
 
 import '../styles/setting.scss';
 import { useNavigate, useParams } from 'react-router-dom';
 import axios from 'axios';
 
-export function SetNewsWrite() {
+import ReactQuill, { Quill } from 'react-quill';
+import { uploadBytes, getDownloadURL, ref } from 'firebase/storage';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+import ImageResize from '@looop/quill-image-resize-module-react';
+import 'react-quill/dist/quill.snow.css';
+import 'react-quill/dist/quill.core.css';
+import '../styles/test.scss';
+
+import { storage } from '../config/Firebase';
+
+// Quill.register('modules/imageResize', ImageResize);
+export const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'align',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'background',
+  'color',
+  'link',
+  'image',
+  'video',
+  'width',
+  'height',
+  'float',
+];
+const imageHandler = (quillRef, storage) => {
+  const input = document.createElement('input');
+  input.setAttribute('type', 'file');
+  input.setAttribute('accept', 'image/*');
+  input.click();
+  input.addEventListener('change', async () => {
+    const editor = quillRef.current.getEditor();
+    const file = input.files[0];
+    // 현재 커서 위치 저장
+    const range = editor.getSelection();
+    // 서버에 올려질때까지 표시할 로딩 placeholder 삽입
+    editor.insertEmbed(range.index, 'image', `/images/loading2.gif`);
+
+    try {
+      // 파일명을 "image/Date.now()"로 저장
+      const storageRef = ref(storage, `image/${Date.now()}`);
+
+      // Firebase Method : uploadBytes, getDownloadURL
+      await uploadBytes(storageRef, file).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          // 이미지 URL 에디터에 삽입
+          editor.deleteText(range.index, 1); //placeholder 삭제
+          editor.insertEmbed(range.index, 'image', url);
+          // URL 삽입 후 커서를 이미지 뒷 칸으로 이동
+          editor.setSelection(range.index + 1);
+        });
+      });
+    } catch (error) {
+      editor.deleteText(range.index, 1);
+    }
+  });
+};
+
+const modules = {
+  toolbar: {
+    container: [
+      [{ header: [1, 2, 3, 4, false] }], // header 설정
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'], // 굵기, 기울기, 밑줄 등 부가 tool 설정
+      [
+        { list: 'ordered' },
+        { list: 'bullet' },
+        { indent: '-1' },
+        { indent: '+1' },
+      ], // 리스트, 인덴트 설정
+      ['link', 'image'], // 링크, 이미지, 비디오 업로드 설정
+      [{ align: [] }, { color: [] }, { background: [] }], // 정렬, 글자 색, 글자 배경색 설정
+      ['clean'], // toolbar 설정 초기화 설정
+    ],
+
+    // 핸들러 설정
+    handlers: {
+      image: imageHandler, // 이미지 tool 사용에 대한 핸들러 설정
+    },
+
+    // 이미지 크기 조절
+    imageResize: {
+      displayStyles: {
+        backgroundColor: 'black',
+        border: 'none',
+        color: 'white',
+      },
+      modules: ['Resize', 'DisplaySize', 'Toolbar'],
+    },
+  },
+};
+export function SetPostWrite({ placeholder, value, ...rest }) {
   const { id } = useParams();
   const navigate = useNavigate();
-  const [newsTitle, setNewsTitle] = useState('');
-  const [newsContent, setNewsContent] = useState('');
+  const quillRef = useRef();
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+  const [postId, setPostId] = useState();
+  //html형식 그대로 넣는거때매 필요
+  const [fetchedContent, setFetchedContent] = useState('');
 
+  //뉴스 추가
   const sendNews = async () => {
     const res = await axios({
       method: 'POST',
@@ -19,17 +124,50 @@ export function SetNewsWrite() {
         Authorization: `Bearer ${localStorage.getItem('token')}`,
       },
       data: {
-        title: newsTitle,
-        content: newsContent,
+        postTitle: title,
+        content: content,
         countryId: id,
       },
     });
     if (res.data.success) {
-      alert('글이 등록되었습니다.');
+      toast('글이 등록되었습니다.');
+      // navigate(`${countryId}/news/read/${postId}`);
     }
   };
+  //뉴스 조회
+  const getNews = async () => {
+    const res = await axios({
+      method: 'GET',
+      url: `http://localhost:8080/api/post/article`,
+      params: { id: postId },
+    });
+    const { content, postTitle } = res.data.result;
 
+    document.querySelector('.ql-editor').innerHTML = content;
+    localStorage.removeItem('postId');
+  };
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      // setUser();
+    } else {
+      // alert('로그인 후 이용해주세요.');
+      // navigate('/signup');
+      // return;
+    }
+    if (localStorage.getItem('postId')) {
+      setPostId(Number(localStorage.getItem('postId')));
+    }
+    if (quillRef.current) {
+      const editor = quillRef.current.getEditor();
+      const toolbar = editor.getModule('toolbar');
+      toolbar.addHandler('image', () => imageHandler(quillRef, storage));
+    }
+  }, []);
   const handleNews = () => {
+    if (!content.trim() || content.trim() === '<p><br></p>') {
+      alert('내용을 입력해주세요');
+      return;
+    }
     try {
       //db에 들어가는 로직
       sendNews();
@@ -43,6 +181,7 @@ export function SetNewsWrite() {
 
   return (
     <>
+      <ToastContainer />
       <div>뉴스 작성</div>
       <form className="box-style">
         <div
@@ -55,37 +194,48 @@ export function SetNewsWrite() {
         >
           {/* 뉴스 제목 */}
           <input
+            className="ql-snow ql-toolbar ql-title"
             type="text"
-            placeholder="뉴스 제목을 입력하세요."
-            value={newsTitle}
-            onChange={(e) => setNewsTitle(e.target.value)}
+            placeholder="제목을 입력하세요."
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             style={{
-              marginBottom: '20px',
-              padding: '10px',
+              marginBottom: '10px',
               border: 'none',
-              borderRadius: '18px',
             }}
           />
           {/* 뉴스 기사 */}
-          <textarea
-            placeholder="뉴스 기사를 입력하세요."
-            value={newsContent}
-            onChange={(e) => setNewsContent(e.target.value)}
+          <ReactQuill
             style={{
-              height: '200px',
-              padding: '10px',
+              height: '300px',
               resize: 'none',
               border: 'none',
               borderRadius: '18px',
             }}
+            {...rest}
+            placeholder={placeholder}
+            theme="snow"
+            ref={quillRef}
+            value={content}
+            onChange={setContent}
+            modules={modules}
+            formats={formats}
           />
 
-          {/* 저장 버튼 */}
-          <ConfirmBtn
-            btnName="작성"
-            backgroundColor="#61759f"
-            onClick={handleNews}
-          ></ConfirmBtn>
+          <div className="postBtn">
+            <ConfirmBtn
+              btnName="취소"
+              onClick={() => navigate(-1)}
+              backgroundColor="#bacd92"
+              width="40%"
+            ></ConfirmBtn>
+            {/* 저장 버튼 */}
+            <ConfirmBtn
+              btnName="작성"
+              backgroundColor="#61759f"
+              onClick={handleNews}
+            ></ConfirmBtn>
+          </div>
         </div>
       </form>
     </>

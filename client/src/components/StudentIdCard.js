6fc8aeede00ae6c63bcf274c650797d3a69a41ff
cci0 <dudtidd4484@gmail.com>
2024-05-20 11:22:17 +0900

    feat/student

--- client/src/components/StudentIdCard.js
+++ client/src/components/StudentIdCard.js
@@ -2,18 +2,20 @@ import React from 'react';
 
 export default function StudentIdCard() {
   return (
-    <div>
+    <div className="idCard-wrap">
       <div className="idCard-title">신분증</div>
-      <div>이미지</div>
-      <div className="idCard-detail">
-        <div className="idCard-detail-title">홍길동</div>
-        <div>
-          <span className="idCard-detail-title">직업</span>
-          <span className="idCard-detail-content">직업종류</span>
-        </div>
-        <div>
-          <span className="idCard-detail-title">신용등급</span>
-          <span className="idCard-detail-content">1등급</span>
+      <div className="idCard-list">
+        <div>이미지</div>
+        <div className="idCard-detail">
+          <div className="idCard-detail-title">홍길동</div>
+          <div className="idCard-detail-list">
+            <span className="idCard-detail-title">직업</span>
+            <span className="idCard-detail-content">직업종류</span>
+          </div>
+          <div className="idCard-detail-list">
+            <span className="idCard-detail-title">신용등급</span>
+            <span className="idCard-detail-content">1등급</span>
+          </div>
         </div>
       </div>
     </div>

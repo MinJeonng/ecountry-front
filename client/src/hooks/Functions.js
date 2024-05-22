cde29918d5f642243ac993b0a8d69899de1b6c7c
cje206 <projec206@gmail.com>
2024-05-22 14:07:55 +0900

    back: 챗봇 생성 중

--- client/src/hooks/Functions.js
+++ client/src/hooks/Functions.js
@@ -29,3 +29,41 @@ export const htmlToText = (html) => {
   }
   return newHtml;
 };
+
+export const getOnlyTime = (time) => {
+  if (time === '') {
+    return '';
+  }
+  const newTime = new Date(time);
+  let result;
+  if (newTime.getHours() > 12) {
+    result = `오후 ${(newTime.getHours() - 12)
+      .toString()
+      .padStart(2, '0')}:${newTime.getMinutes()}`;
+  } else {
+    result = `오전 ${newTime
+      .getHours()
+      .toString()
+      .padStart(2, '0')}:${newTime.getMinutes()}`;
+  }
+  console.log(result);
+  return result;
+};
+
+export const compareTime = (prevTime, nowTime) => {
+  const prevDate = new Date(prevTime);
+  const nowDate = new Date(nowTime);
+  if (
+    prevDate.getFullYear() === nowDate.getFullYear() &&
+    prevDate.getMonth() === nowDate.getMonth() &&
+    prevDate.getDate() === nowDate.getDate() &&
+    prevDate.getHours() === nowDate.getHours() &&
+    prevDate.getMinutes() === nowDate.getMinutes()
+  ) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+};

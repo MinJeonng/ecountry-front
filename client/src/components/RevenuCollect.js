d3796ead87cbbee2a6a4d016037318c17bbf2c21
cje206 <projec206@gmail.com>
2024-05-25 13:26:07 +0900

    back: 과태료 징수 연결:

--- client/src/components/RevenuCollect.js
+++ client/src/components/RevenuCollect.js
@@ -7,12 +7,26 @@ import axios from 'axios';
 
 export default function RevenuCollect() {
   const { id } = useParams();
+  const [accountId, setAccountId] = useState('');
+  const [selectTax, setSelectTax] = useState('');
+  const [unit, setUnit] = useState('');
+  const [transaction, setTransaction] = useState(0);
   const [studentList, setStudentList] = useState([]);
   const [taxList, setTaxList] = useState([]);
   // 학생리스트 사유-징수금 선택해서 징수할 수 있게 만들면됨...
   // 학생리스트
 
-  const getStudent = async () => {};
+  const getStudent = async () => {
+    const res = await axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_HOST}/api/bank/students/${id}`,
+      headers: {
+        'Content-Type': `application/json`,
+        'ngrok-skip-browser-warning': '69420',
+      },
+    });
+    setStudentList(res.data.result);
+  };
 
   const getPenalty = async () => {
     const res = await axios({
@@ -23,11 +37,53 @@ export default function RevenuCollect() {
         'ngrok-skip-browser-warning': '69420',
       },
     });
-    console.log(res.data.result);
+    setTaxList(res.data.result);
+  };
+
+  const collectionTax = async () => {
+    const res = await axios({
+      method: 'POST',
+      url: `${process.env.REACT_APP_HOST}/api/tax/penalty/${id}`,
+      data: {
+        transaction,
+        memo: selectTax,
+        withdrawId: accountId,
+      },
+      headers: {
+        'Content-Type': `application/json`,
+        'ngrok-skip-browser-warning': '69420',
+      },
+    });
+  };
+
+  const getUnit = async () => {
+    const res = await axios({
+      method: 'GET',
+      url: `${process.env.REACT_APP_HOST}/api/bank/unit/${id}`,
+      headers: {
+        'Content-Type': `application/json`,
+        'ngrok-skip-browser-warning': '69420',
+      },
+    });
+    setUnit(res.data.result.unit);
+  };
+
+  const getTaxMoney = () => {
+    taxList.forEach((tax) => {
+      if (tax.taxName === selectTax) {
+        setTransaction(tax.tax);
+      }
+    });
   };
 
   useEffect(() => {
+    getTaxMoney();
+  }, [selectTax]);
+
+  useEffect(() => {
     getPenalty();
+    getStudent();
+    getUnit();
   }, []);
   return (
     <>
@@ -45,6 +101,8 @@ export default function RevenuCollect() {
       <form className="box-style">
         <div className="set-title">징수 대상자</div>
         <select
+          value={accountId}
+          onChange={(e) => setAccountId(e.target.value)}
           style={{
             width: '100%',
             height: '30px',
@@ -55,9 +113,20 @@ export default function RevenuCollect() {
             margin: '10px 0 20px 0',
             outline: 'none',
           }}
-        ></select>
+        >
+          <option value="" disabled>
+            대상자 선택
+          </option>
+          {studentList.map((student) => (
+            <option key={student.id} value={student.id}>
+              {student.rollNumber}번 {student.name}
+            </option>
+          ))}
+        </select>
         <div className="set-title">징수 사유</div>
         <select
+          value={selectTax}
+          onChange={(e) => setSelectTax(e.target.value)}
           style={{
             width: '100%',
             height: '30px',
@@ -68,7 +137,16 @@ export default function RevenuCollect() {
             margin: '10px 0 20px 0',
             outline: 'none',
           }}
-        ></select>
+        >
+          <option value="" disabled>
+            사유 선택
+          </option>
+          {taxList.map((tax, index) => (
+            <option key={index} value={tax.taxName}>
+              {tax.taxName}
+            </option>
+          ))}
+        </select>
         <div className="set-title">징수 금액</div>
         <div
           style={{
@@ -82,7 +160,7 @@ export default function RevenuCollect() {
             position: 'relative',
           }}
         >
-          <span>10000</span>
+          <span>{transaction}</span>
           <span
             style={{
               color: '#a5a5a5',
@@ -92,11 +170,15 @@ export default function RevenuCollect() {
               marginRight: '9%',
             }}
           >
-            단위
+            {unit}
           </span>
         </div>
       </form>
-      <ConfirmBtn btnName="징수" backgroundColor="#61759f"></ConfirmBtn>
+      <ConfirmBtn
+        btnName="징수"
+        backgroundColor="#61759f"
+        onClick={collectionTax}
+      ></ConfirmBtn>
     </>
   );
 }

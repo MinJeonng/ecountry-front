dfff3a44a693f3f30c4032a03fe2175d20849770
cci0 <dudtidd4484@gmail.com>
2024-05-29 11:43:33 +0900

    fix/회원가입 및 로그인 toast 문제

--- client/src/components/Template.js
+++ client/src/components/Template.js
@@ -1,26 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Container = styled.div`
+  @media (min-width: 1160px) {
+    display: flex;
+    justify-content: center;
+  }
+`;
+
+const Content = styled.div`
+  @media (min-width: 1160px) {
+    width: 1100px;
+  }
+`;
+
 const BgTop = styled.div`
-  background: linear-gradient(to left, #75a47f, #bacd92);
-  height: 300px;
-  position: relative;
+  @media (max-width: 1159px) {
+    background: linear-gradient(to left, #75a47f, #bacd92);
+    height: 300px;
+    position: relative;
+  }
+
+  @media (min-width: 1160px) {
+    display: none;
+  }
 `;
 
 const BgBottom = styled.div`
-  position: relative;
-  margin-top: -125px;
-  padding: 40px 30px 90px;
-  background-color: #ffffff;
-  border-top-left-radius: 25px;
-  border-top-right-radius: 25px;
+  @media (max-width: 1159px) {
+    position: relative;
+    margin-top: -125px;
+    padding: 40px 30px 90px;
+    background-color: #ffffff;
+    border-top-left-radius: 25px;
+    border-top-right-radius: 25px;
+  }
+
+  @media (min-width: 1160px) {
+    width: 1100px;
+  }
 `;
 
 export default function Template({ childrenTop, childrenBottom }) {
   return (
-    <>
-      <BgTop>{childrenTop}</BgTop>
-      <BgBottom>{childrenBottom}</BgBottom>
-    </>
+    <Container>
+      <Content>
+        <BgTop>{childrenTop}</BgTop>
+        <BgBottom>{childrenBottom}</BgBottom>
+      </Content>
+    </Container>
   );
 }

5effaa11b614f0d0ad5cafa40550fd64934c9cd8
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-06-05 00:54:59 +0900

    일부 css 수정

--- client/src/store/studentInfoReducer.js
+++ client/src/store/studentInfoReducer.js
@@ -1,28 +1,3 @@
-// export const STUDENT_INFO_LIST = 'STUDENT_INFO_LIST';
-
-// export const setStudentInfoList = (studentInfoList) => ({
-//   type: STUDENT_INFO_LIST,
-//   payload: studentInfoList,
-// });
-
-// const initialState = {
-//   studentInfoList: [],
-// };
-// const studentInfoReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case STUDENT_INFO_LIST:
-//       return {
-//         ...state,
-//         studentInfoList: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-
-// };
-
-// export default studentInfoReducer;
-import { PURGE } from 'redux-persist';
 export const STUDENT_INFO_LIST = 'STUDENT_INFO_LIST';
 
 export const setStudentInfoList = (studentInfoList) => ({
@@ -33,7 +8,6 @@ export const setStudentInfoList = (studentInfoList) => ({
 const initialState = {
   studentInfoList: [],
 };
-
 const studentInfoReducer = (state = initialState, action) => {
   switch (action.type) {
     case STUDENT_INFO_LIST:
@@ -41,8 +15,6 @@ const studentInfoReducer = (state = initialState, action) => {
         ...state,
         studentInfoList: action.payload,
       };
-    case PURGE:
-      return initialState; // PURGE 액션에 대해 초기 상태를 반환하여 리듀서를 초기화합니다.
     default:
       return state;
   }
